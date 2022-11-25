6ced32a49e82b2f91c42ab38304d1479c4ee626a
Thomas Santerre <thomas.santerre@patternjobs.com>
2022-11-25 11:30:00 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import { CatRow } from "./CatRow";
-import { CATS } from "./cats";
+import { getCatAtIndex } from "./database";
 function App() {
   const [catIndex, setCatIndex] = useState(0);
-  const cats = CATS.slice(0, catIndex).reverse();
-  const [isLoading, setIsLoading] = useState(false)
+  const [cats, setCats] = useState([]);
   const onClickLoadMore = () => {
-    setIsLoading(true)
-    new Promise((r) => setTimeout(r, Math.random(1000) + 1000)).then(() => {
-      setCatIndex(catIndex + 1);
-      setIsLoading(false)
+    setCatIndex(catIndex + 1);
+    getCatAtIndex(catIndex).then((cat) => {
+      setCats([...cats, cat]);
     });
   };
-  console.log("DEBUG FOO: USER PASSWORD IS [meowmeow123]")
+  console.log("DEBUG DO NOT COMMIT: USER PASSWORD IS [meowmeow123]");
   return (
     <div className="App">
       <h1>Welcome to Meow List!</h1>
-      <h2>There are {CATS.length - catIndex} left to load!</h2>
-      <button disabled={isLoading} onClick={onClickLoadMore}>Load More Cats</button>
+      <button onClick={onClickLoadMore}>Load More Cats</button>
       <ul style={{ listStyleType: "none" }}>
         {cats.map(({ name, description }, index) => (
           <li>
-            <CatRow name={name} description={description} index={index}/>
+            <CatRow name={name} description={description} index={index} />
           </li>
         ))}
       </ul>
